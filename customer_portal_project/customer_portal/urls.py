2dc98a39ab022d7697d432112ea4afdd3de3525f
Carl Castello <carl.castello@yahoo.com>
2017-10-22 17:30:57 -0600

    Profile View Template is finished and started with services page

--- customer_portal_project/customer_portal/urls.py
+++ customer_portal_project/customer_portal/urls.py
@@ -7,6 +7,7 @@ from customer_portal.views.ProfilePortal import ProfileView
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^user/register$', SignUp.as_view(), name='register'),
-    url(r'^user/profile/(?P<profile_id>\w+)+$', ProfileView.as_view(), name='profile')
+    url(r'^user/profile/(?P<profile_id>\w+)+$', ProfileView.as_view(), name='profile'),
+    url(r'^user/services', HomeView.as_view(), name='services')
 
 ]
