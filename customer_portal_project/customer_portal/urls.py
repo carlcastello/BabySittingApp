6538086bc6eb0ad1e47be429f04008541468ea79
Carl Castello <carl.castello@yahoo.com>
2017-11-11 13:38:26 -0600

    Service Page and saving.

--- customer_portal_project/customer_portal/urls.py
+++ customer_portal_project/customer_portal/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from customer_portal.views.CustomerPortal import HomeView
+from customer_portal.views.CustomerPortal import HomeView, ServicesView
 from customer_portal.views.Authentication import SignUp
 from customer_portal.views.ProfilePortal import ProfileView
 
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^user/register$', SignUp.as_view(), name='register'),
     url(r'^user/profile/(?P<profile_id>\w+)+$', ProfileView.as_view(), name='profile'),
-    url(r'^user/services', HomeView.as_view(), name='services')
+    url(r'^user/services', ServicesView.as_view(), name='services')
 
 ]
--- customer_portal_project/customer_portal/views/CustomerPortal.py
+++ customer_portal_project/customer_portal/views/CustomerPortal.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "customer_portal/home.html"
+
+
+class ServicesView(TemplateView):
+    template_name = "customer_portal/services.html"
